921b3f342e53be846b22494a94109e238846fe60
vkovanusic <v.kovanusic@conreal.ch>
2022-09-11 03:20:18 +0200

    asdad

--- src/main/web/src/app/app.module.ts
+++ src/main/web/src/app/app.module.ts
@@ -26,24 +26,24 @@ import {DetailsTaskComponent} from './pages/task/details-task/details-task.compo
 let toolbar = [
 
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['blockquote', 'code-block'],
+ // ['blockquote', 'code-block'],
 
   [{'header': 1}, {'header': 2}],               // custom button values
   [{'list': 'ordered'}, {'list': 'bullet'}],
-  [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
-  [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
-  [{'direction': 'rtl'}],                         // text direction
+ // [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
+//  [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
+//  [{'direction': 'rtl'}],                         // text direction
 
-  [{'size': ['small', false, 'large', 'huge']}],  // custom dropdown
-  [{'header': [1, 2, 3, 4, 5, 6, false]}],
+//  [{'size': ['small', false, 'large', 'huge']}],  // custom dropdown
+//  [{'header': [1, 2, 3, 4, 5, 6, false]}],
 
   [{'color': []}, {'background': []}],          // dropdown with defaults from theme
-  [{'font': []}],
-  [{'align': []}],
+//  [{'font': []}],
+ // [{'align': []}],
 
-  ['clean'],                                         // remove formatting button
+//  ['clean'],                                         // remove formatting button
 
-  ['link', 'image', 'video']                         // link and image, video
+  ['link', 'image',/* 'video'*/]                         // link and image, video
 ];
 
 @NgModule({
